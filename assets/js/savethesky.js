338f95c857138d75b25d7e0d94090276fae6ee6a
RicardoSe14 <Ricardo.s14@gmail.com>
2023-05-12 15:00:37 -0400

    Revisión Final

---
+++ assets/js/savethesky.js
@@ -0,0 +1,146 @@
+
+
+$("#mvvideo").click(function(){
+    $('.titulo1, .titulo2, #fondo, #galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
+});
+
+$("#mvvideo").click(function(){
+    $('#fondo3').show(0);
+});
+
+$("#minicio").click(function(){
+    $(".titulo1, .titulo2, #fondo").show(0);
+});
+
+$("#minicio").click(function(){
+    $('#galeria, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
+});
+  
+
+$("#mgaleria").click(function(){
+    $("#galeria, #fondo").show(0);
+});
+
+$("#mgaleria").click(function(){
+    $('.titulo1, .titulo2, #contacto, #ubicacion, .zoom, #fondo2').hide(0);
+});
+
+$("#mubicacion").click(function(){
+    $("#ubicacion, #fondo").show(0);
+});
+
+$("#mubicacion").click(function(){
+    $('.titulo1, .titulo2, #contacto, #galeria, .zoom, #fondo2').hide(0);
+});
+
+$("#mcontacto").click(function(){
+    $("#contacto, #fondo").show(0);
+});
+
+$("#mcontacto").click(function(){
+    $('.titulo1, .titulo2, #galeria, .zoom, #ubicacion, #fondo2').hide(0);
+});
+
+$("#mapa").click(function(){
+    $('#ubicacion, #galeria, #contacto, .zoom, #fondo2').hide(0);
+});
+
+$("#mapa").click(function(){
+    $('.titulo1, .titulo2, #fondo').show(0);
+});
+
+$("#tarjeta1").click(function(){
+    $("#t1, #fondo2").show(0);
+});
+
+$("#tarjeta1").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t1").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t1").click(function(){
+    $("#galeria").show(0);
+});
+
+$("#tarjeta2").click(function(){
+    $("#t2 , #fondo2").show(0);
+});
+
+$("#tarjeta2").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t2").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t2").click(function(){
+    $("#galeria").show(0);
+});
+
+$("#tarjeta3").click(function(){
+    $("#t3, #fondo2").show(0);
+});
+
+$("#tarjeta3").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t3").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t3").click(function(){
+    $("#galeria").show(0);
+});
+
+$("#tarjeta4").click(function(){
+    $("#t4, #fondo2").show(0);
+});
+
+$("#tarjeta4").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t4").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t4").click(function(){
+    $("#galeria").show(0);
+});
+
+$("#tarjeta5").click(function(){
+    $("#t5, #fondo2").show(0);
+});
+
+$("#tarjeta5").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t5").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t5").click(function(){
+    $("#galeria").show(0);
+});
+
+$("#tarjeta6").click(function(){
+    $("#t6, #fondo2").show(0);
+});
+
+$("#tarjeta6").click(function(){
+    $('#galeria').hide(0);
+});
+
+$("#t6").click(function(){
+    $('.zoom, #fondo2').hide(0);
+});
+
+$("#t6").click(function(){
+    $("#galeria").show(0);
+});
